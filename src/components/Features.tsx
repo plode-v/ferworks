83a91ac55d9a1cc283f244f7534c18bdfac55704
Plode <84858705+plode-v@users.noreply.github.com>
2023-10-26 20:09:48 +0700

    add navbar hide on scroll component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -5,15 +5,9 @@ const Features = () => {
   return (
     <div className='flex w-full h-[350px] justify-center'>
         <div className='w-[50%] h-full justify-evenly items-center flex'>
-            <FeatureComponent text="Lorem ipsum dolor sit amet, consectetur adipiscing elit" />
-            <div className='flex flex-col items-center w-[30%] p-5 text-center justify-center'>
-                <div className='border-black border rounded-full h-[80px] w-[80px]' />
-                <p className='py-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-            </div>
-            <div className='flex flex-col items-center w-[30%] p-5 text-center justify-center'>
-                <div className='border-black border rounded-full h-[80px] w-[80px]' />
-                <p className='py-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-            </div>
+            <FeatureComponent text="Simple text for feature #1" />
+            <FeatureComponent text="Simple text for feature #2" />
+            <FeatureComponent text="Simple text for feature #3" />
         </div>
     </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+'use client'
+import React, { useState, useEffect } from 'react'
 
 const Navbar = () => {
+
+  const [prevScroll, setPrevScroll] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentPosition = window.scrollY;
+    const scrollingDown = currentPosition > prevScroll;
+
+    setVisible(!scrollingDown);
+    setPrevScroll(currentPosition);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  })
+
   return (
-    <div className='h-[70px] justify-between flex items-center px-5 bg-blue-500 text-white w-[98%] -translate-x-1/2 left-1/2 top-3 fixed z-20 rounded-md'>
+    <div className={`h-[70px] justify-between flex items-center px-5 bg-blue-500 text-white w-[98%] fixed z-20 rounded-md top-3 left-1/2 -translate-x-1/2 duration-300 ${visible ? 'transform translate-y-0' : '-translate-y-[calc(0.75rem+70px)]'}`}>
         <div>
             Logo
         </div>
