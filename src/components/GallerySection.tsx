e5bd4ae3f99488b249482b720623aa54ea2410b5
Plode <84858705+plode-v@users.noreply.github.com>
2023-10-28 20:12:11 +0700

    Add carousel in gallery section

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -1,9 +1,28 @@
+'use client'
 import React from 'react'
+import Carousel from 'nuka-carousel'
+import Image from 'next/image'
 
 const GallerySection = () => {
+
   return (
-    <div className='w-full h-[400px] bg-neutral-50'>
-        Gallery
+    <div className='w-full h-[700px] bg-neutral-50'>
+        <div className='flex items-center justify-center h-full'>
+          <Carousel
+            autoplay={true}
+            wrapAround={true}
+            cellAlign='left'
+            style={{ width: '80vw', height: "700px"}}
+            enableKeyboardControls={true}
+            dragging={false}
+            >
+              <div className='h-[700px] w-full'>
+                <Image src="/public/img-1.jpg" alt="img-1" width={100} height={700}/>
+              </div>
+              <Image src="/public/img-2.jpg" alt='img-2' width={500} height={700} />
+            <div className="w-full h-full bg-purple-500">3</div>
+          </Carousel>
+        </div>
     </div>
   )
 }
