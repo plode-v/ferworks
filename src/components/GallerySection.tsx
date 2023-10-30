d170ca9028c4c885ac8f2a16e4377278fc1d2a4b
Plode <84858705+plode-v@users.noreply.github.com>
2023-10-30 12:57:31 +0700

    Update carousel

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -9,15 +9,17 @@ import image3 from '/public/img-3.jpg'
 const GallerySection = () => {
 
   return (
-    <div className='w-full h-[700px] bg-neutral-50'>
-      <Carousel
-       className='flex items-center justify-center w-full' 
-      >
-        <Carousel.Item>
-          <Image src={image1} alt='image-1' width={1200} height={700} />
+    <div className='w-full h-[700px] bg-neutral-50 flex justify-center'>
+      <Carousel className='w-full' variant='dark'>
+        <Carousel.Item className='object-contain'>
+          <div className='flex justify-center w-full'>
+            <Image src={image1} alt='image-1' height={700} className='object-cover'/>
+          </div>
         </Carousel.Item>
-        <Carousel.Item>
-          <Image src={image2} alt='image-2' width={500} height={700} />
+        <Carousel.Item className=''>
+          <div className='flex justify-center'>
+            <Image src={image2} alt='image-2' height={700} />
+          </div>
         </Carousel.Item>
       </Carousel>
 
