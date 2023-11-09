174e19550417d325abf10b9e865ffd477fda7a60
Plode <84858705+plode-v@users.noreply.github.com>
2023-11-09 22:20:45 +0700

    add blob component

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Blob from '@/components/Blob'
 
 const page = () => {
   return (
     <div className='min-h-screen h-screen w-full'>
       <div className='relative h-2/3 w-full bg-neutral-200 overflow-hidden'>
-        <div className='bg-gradient-to-tl from-[#429EF5] to-green-500 absolute -left-32 -bottom-40 w-[500px] h-[500px] rounded-full blur-2xl animate-spin-slow'></div>
+        <Blob />
         <div className='bg-gradient-to-tr from-purple-500 to-orange-500 absolute -right-60 -top-96 w-[700px] h-[700px] rounded-full blur-3xl animate-spin-slow'></div>
         <h1 className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[60px] uppercase font-[200]'>contact us</h1>
       </div>
