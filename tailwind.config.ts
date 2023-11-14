e56bf3179f4de7730dfce373220177c88abb67f0
Plode <84858705+plode-v@users.noreply.github.com>
2023-11-14 10:41:01 +0700

    update spin animation duration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,7 +69,7 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "spin-slow": "spin 3s linear infinite"
+        "spin-slow": "spin 2300ms linear infinite"
       },
     },
   },
