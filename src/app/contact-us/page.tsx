d3c7d955f03f92e5c965a072ab9f510b6751b390
Plode <84858705+plode-v@users.noreply.github.com>
2023-11-16 11:12:53 +0700

    update contact us page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+'use client'
+import React, { ChangeEvent, FormEvent, FormEventHandler, LegacyRef, useRef, useState } from 'react'
 import Header from '@/components/Header'
 import ContactCard from '@/components/ContactCard'
+import emailjs from "@emailjs/browser";
 
-const page = () => {
+const ContactUs = () => {
+
+  const form = useRef();
+
+  const sendEmail = (event: any) => {
+    event.preventDefault();
+
+    emailjs.sendForm("YOUR SERVICE ID", "YOUR TEMPLATE ID", form.current!, "YOUR PUBLIC KEY")
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      })
+  }
+
   return (
     <div className='h-screen w-full bg-white'>
       <Header header='contact us' />
@@ -26,7 +42,21 @@ const page = () => {
         </div>
         <section className='2xl:h-[800px] h-[600px] w-full items-center flex justify-center 3xl:w-2/3'>
           <div className='flex border h-[85%] w-4/5 items-center justify-center'>
-            contact form
+            <form ref={form} onSubmit={sendEmail} className='flex flex-col gap-4'>
+              <div className='grid'>
+                <label>Name:</label>
+                <input type="text" name='name' className='border' />
+              </div>
+              <div className='grid'>
+                <label>Email:</label>
+                <input type="email" name='email' className='border' />
+              </div>
+              <div className='grid'>
+                <label>Message:</label>
+                <textarea name="message" className='border' />
+              </div>
+              <button type='submit' className='bg-blue-500 text-white rounded-sm'>Send</button>
+            </form>
           </div>
         </section>
       </div>
@@ -34,4 +64,4 @@ const page = () => {
   )
 }
 
-export default page
+export default ContactUs
