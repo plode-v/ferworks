a8080463c16be7614886e6a0e45c0f5c0ce9879d
Plode <84858705+plode-v@users.noreply.github.com>
2024-02-02 22:38:23 +0700

    add logo to the navbar

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -6,8 +6,9 @@ const Introduction = () => {
     <div className='w-full lg:h-[700px] md:h-[340px] flex-col flex items-center justify-center'>
       <div className='w-full duration-200 3xl:w-1/2 h-full flex items-center flex-col bg-neutral-200 rounded-xl'>
         <div className='h-full w-3/4 duration-300 flex flex-col gap-5 rounded-2xl justify-start pt-16'>
-          <h1 className='text-[42px] font-black text-black/90 w-[55%] leading-[42px]'>Say something here usually something catchy</h1>
-          <p className='text-black/80'>this is usually something that explains it. Can be about this long.</p>
+          {/* FIXME: change intro */}
+          <h1 className='text-[42px] font-black text-black/90 w-[55%] leading-[42px] bg-orange-500'>Say something here usually something catchy</h1>
+          <p className='text-black/80 w-max bg-orange-500'>this is usually something that explains it. Can be about this long.</p>
         </div> 
         <Features />
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 'use client'
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react'
-import { Button } from './ui/button';
-import { BiSolidPhoneCall } from "react-icons/bi";
+import logo from "../../public/ferworks-logo.png";
+import Image from 'next/image';
 
 const Navbar = () => {
 
@@ -29,7 +29,7 @@ const Navbar = () => {
       <div className={`lg:h-[100px] md:h-[50px] justify-center flex items-center border-b lg:px-16 border-black/20 w-[97%] 3xl:w-1/2 fixed z-30 3xl:justify-center backdrop-blur-lg bg-neutral-50/80`}>
         <div className='flex w-full items-center justify-between h-full'>
           <Link className='text-black no-underline' href="/">
-            <div>Logo</div>
+            <Image src={logo} alt="logo" className='lg:h-[40px] w-auto' />
           </Link>
           <div className='flex gap-5 items-center uppercase font-[500] tracking-tight text-black lg:text-[14px] md:text-[12px]'>
             <p className='font-[200] tracking-wider'>(949) 430-1028</p>
