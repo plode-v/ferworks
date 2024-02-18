f81aa497e333e769b9859485fc5e90f42f60d1e5
Plode <84858705+plode-v@users.noreply.github.com>
2024-02-18 13:58:36 +0700

    setup framer motion on herobanner

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,18 +1,74 @@
 'use client'
 import React from 'react'
 import BookingModal from './BookingModal';
+import { motion, Variants } from "framer-motion";
 
 const HeroBanner = () => {
-  // FIXME: INTRO ANIMATION (RUNNING CHARACTER WHEN LOAD)
+
+  const text = 'Your safety is our'
+  const splitText = text.split(" ");
+
+  const priority = "priority."
+  const splitPriority = priority.split("");
+
+  const textContainer: Variants = {
+    hidden: {
+      opacity: 0,
+      transition: {
+        duration: 0
+      }
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.03,
+      }
+    }
+  }
+
+  const textVariant = {
+    hidden: {
+      opacity: 0,
+      y: -10,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    }
+  }
+  
   return (
     <div className='h-[40vh] md:h-[400px] lg:h-[50vh] xl:h-screen 2xl:h-[750px] w-full max-w-screen overflow-hidden bg-neutral-50 flex flex-col items-center justify-center lg:pt-[100px] md:pt-[50px]'>
       <div className='h-full w-full 3xl:w-1/2 duration-200'>
         <div className='h-full w-full flex items-center justify-center flex-col md:gap-5 gap-2'>
-          <div className='flex items-center justify-center md:gap-4 gap-2 lg:text-[54px] md:text-[24px] 3xl:text-[62px] text-[20px]'>
-            <h1 className='text-black/80 font-[800] md:tracking-wider'>Your safety is our</h1>
-            <h1 className='bg-gradient-to-r uppercase from-cyan-400 to-blue-700 text-transparent bg-clip-text font-[900] md:tracking-wider tracking-wide'>priority.</h1>
-          </div>
-          <p className='md:w-max w-full text-center text-black/90 lg:text-[18px] md:text-[14px] text-[11px]'>contact us now to explore reliable, budget-friendly security options. <br /> Your safety and the functionality of your devices are our priorities!</p>
+          <motion.div 
+            className='flex items-center justify-center lg:text-[54px] md:text-[24px] 3xl:text-[62px] text-[20px] gap-3'
+            initial="hidden"
+            animate="visible"
+            variants={textContainer}
+          >
+            {splitText.map((letter, idx) => (
+              <motion.h1
+                key={idx}
+                className='font-[800] text-black/80'
+                variants={textVariant}
+              >
+                {letter}
+              </motion.h1>
+            ))}
+            <motion.div className='flex'>
+              {splitPriority.map((letter, idx) => (
+                <motion.h1
+                  key={idx}
+                  className='md:tracking-wider tracking-wide font-[900] uppercase text-sky-500'
+                  variants={textVariant}
+                >
+                  {letter}
+                </motion.h1>
+              ))}
+            </motion.div>
+          </motion.div>
+          <p className='md:w-max w-full text-center text-black/90 lg:text-[18px] md:text-[14px] text-[11px]'>contact us now to explore reliable, budget-friendly security options. <br /> Your&#8201;safety and the functionality of your devices are our priorities!</p>
           <p></p>
         <BookingModal 
           buttonClass='bg-blue-500 text-white lg:py-4 lg:px-7 md:py-3 md:px-4 py-1 px-4 rounded-full booking-section booking-left after:rounded-[10rem] md:text-[12px] lg:text-[18px] text-[14px]'
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,24 +34,63 @@ const Navbar = () => {
     }
   }
 
+  const container = {
+    hidden: {
+      opacity: 0,
+      y: -10,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.1
+      }
+    }
+  }
+
+  const item = {
+    hidden: {
+      y: -10,
+      opacity: 0
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1
+      }
+    }
+  }
+
   return (
     <div className='w-full flex items-top justify-center h-full relative'>
-      <div className='hidden lg:h-[100px] h-[50px] justify-center md:flex items-center border-b lg:px-16 border-black/20 w-[97%] 3xl:w-1/2 fixed z-30 3xl:justify-center backdrop-blur-lg bg-neutral-50/80'>
-        <div className='flex w-full items-center justify-between h-full'>
+      <motion.div 
+        className='hidden lg:h-[100px] h-[50px] justify-center md:flex items-center border-b lg:px-16 border-black/20 w-[97%] 3xl:w-1/2 fixed z-30 3xl:justify-center backdrop-blur-lg bg-neutral-50/80'
+        initial="hidden"
+        animate="visible"
+        variants={container}
+      >
+        <motion.div
+          className='flex w-full items-center justify-between h-full'
+          variants={item}
+        >
           <Link className='text-black no-underline' href="/">
             <Image src={logo} alt="logo" className='md:h-[20px] lg:h-[40px] w-auto' />
           </Link>
-          <div className='flex lg:gap-5 md:gap-3 items-center uppercase font-[500] tracking-tight text-black lg:text-[14px] md:text-[10px]'>
-            <a className='font-[200] tracking-wider' href='tel:+19494301028'>(949) 430-1028</a>
+          <motion.div 
+            className='flex lg:gap-5 md:gap-3 items-center uppercase font-[500] tracking-tight text-black lg:text-[14px] md:text-[10px]'
+            variants={item}
+          >
+            <motion.a className='font-[200] tracking-wider' href='tel:+19494301028' variants={item}>(949) 430-1028</motion.a>
             <Link href="about-us" className='md:text-[10px] lg:text-[14px]'>
-              About Us
+              <motion.p variants={item}>About Us</motion.p>
             </Link>
             <Link href="contact-us" className='md:text-[10px] lg:text-[14px]'>
-              Contact Us
+              <motion.p variants={item}>Contact Us</motion.p>
             </Link>
-          </div>
-        </div>
-      </div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
       <motion.div
         initial={false}
         animate={isOpen ? "open" : "closed"}
